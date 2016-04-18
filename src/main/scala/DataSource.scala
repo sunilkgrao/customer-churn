d6be65a81eb93334ac3c2a79f6e8d586750378b8
Prasad Rao <Rao@Durgas-iMac.local>
2016-04-17 20:55:24 -0400

    Structure Change

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -4,6 +4,7 @@ import io.prediction.data.storage.Event
 import io.prediction.data.store.PEventStore
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.joda.time.format.DateTimeFormat
 
 case class DataSourceParams(appName: String) extends Params
 
@@ -27,23 +28,48 @@ class DataSource(val dsp: DataSourceParams)
         event.event match {
           case "customer" =>
             Customer(Some(event.entityId),
-              Some(event.properties.get[Boolean]("intl_plan")),
-              Some(event.properties.get[Boolean]("voice_mail_plan")),
-              Some(event.properties.get[Long]("num_vmail_msg")),
-              Some(event.properties.get[Double]("total_day_mins")),
-              Some(event.properties.get[Long]("total_day_calls")),
-              Some(event.properties.get[Double]("total_day_charge")),
-              Some(event.properties.get[Double]("total_eve_mins")),
-              Some(event.properties.get[Long]("total_eve_calls")),
-              Some(event.properties.get[Double]("total_eve_charge")),
-              Some(event.properties.get[Double]("total_night_mins")),
-              Some(event.properties.get[Long]("total_night_calls")),
-              Some(event.properties.get[Double]("total_night_charge")),
-              Some(event.properties.get[Double]("total_intl_mins")),
-              Some(event.properties.get[Long]("total_intl_calls")),
-              Some(event.properties.get[Double]("total_intl_charge")),
-              Some(event.properties.get[Long]("customer_service_calls")),
-              Some(event.properties.get[Boolean]("churn")))
+              Some(event.properties.get[String]("AccountId")),
+              Some(event.properties.get[String]("Name")),
+              Some(event.properties.get[String]("Description")),
+              Some(event.properties.get[Double]("Amount")),
+              Some(event.properties.get[DateTimeFormat]("CloseDate")),
+              Some(event.properties.get[String]("Type")),
+              Some(event.properties.get[String]("NextStep")),
+              Some(event.properties.get[String]("LeadSource")),
+              Some(event.properties.get[Boolean]("IsWon")),
+              Some(event.properties.get[String]("CampaignId")),
+              Some(event.properties.get[String]("OwnerId")),
+              Some(event.properties.get[String]("Territory2Id")),
+              Some(event.properties.get[DateTimeFormat]("CreatedDate")),
+              Some(event.properties.get[String]("FiscalQuarter")),
+              Some(event.properties.get[String]("FiscalYear")),
+              Some(event.properties.get[Double]("Discount__c")),
+              Some(event.properties.get[String]("Competitor__c")),
+              Some(event.properties.get[Double]("Age__c")))
+
+/*
+            sb.append("Id=" + Id);
+            sb.append(",AccountId=" + AccountId);
+            sb.append(",Name=" + Name);
+            sb.append(",Description=" + Description);
+            sb.append(",Amount=" + String.valueOf(Amount));
+            sb.append(",CloseDate=" + String.valueOf(CloseDate));
+            sb.append(",Type=" + Type);
+            sb.append(",NextStep=" + NextStep);
+            sb.append(",LeadSource=" + LeadSource);
+            sb.append(",IsWon=" + String.valueOf(IsWon));
+            sb.append(",CampaignId=" + CampaignId);
+            sb.append(",OwnerId=" + OwnerId);
+            sb.append(",Territory2Id=" + Territory2Id);
+            sb.append(",CreatedDate=" + String.valueOf(CreatedDate));
+            sb.append(",FiscalQuarter=" + String.valueOf(FiscalQuarter));
+            sb.append(",FiscalYear=" + String.valueOf(FiscalYear));
+            sb.append(",Discount__c=" + String.valueOf(Discount__c));
+            sb.append(",Competitor__c=" + Competitor__c);
+            sb.append(",Age__c=" + String.valueOf(Age__c));
+            sb.append("]");
+
+*/
           case _ => throw new Exception(s"Unexpected event ${event} is read.")
         }
       } catch {
@@ -60,25 +86,25 @@ class DataSource(val dsp: DataSourceParams)
 }
 
 @SerialVersionUID(9129684718267757690L) case class Customer(
-                                                             id: Option[String],
-                                                             intlPlan: Option[Boolean],
-                                                             voiceMailPlan: Option[Boolean],
-                                                             numVmailMsg: Option[Long],
-                                                             totalDayMins: Option[Double],
-                                                             totalDayCalls: Option[Long],
-                                                             totalDayCharge: Option[Double],
-                                                             totalEveMins: Option[Double],
-                                                             totalEveCalls: Option[Long],
-                                                             totalEveCharge: Option[Double],
-                                                             totalNightMins: Option[Double],
-                                                             totalNightCalls: Option[Long],
-                                                             totalNightCharge: Option[Double],
-                                                             totalIntlMins: Option[Double],
-                                                             totalIntlCalls: Option[Long],
-                                                             totalIntlCharge: Option[Double],
-                                                             customerServiceCalls: Option[Long],
-                                                             churn: Option[Boolean]
-                                                             ) extends Serializable
+   id: Option[String],
+   accountId: Option[String],
+   name: Option[String],
+   description: Option[String],
+   amount: Option[Double],
+   closeDate: Option[DateTimeFormat],
+   opportunityType: Option[String],
+   nextStep: Option[String],
+   leadSource: Option[String],
+   isWon: Option[Boolean],
+   campaignId: Option[String],
+   ownerId: Option[String],
+   territory: Option[String],
+   createdDate: Option[DateTimeFormat],
+   fiscalQuarter: Option[String],
+   fiscalYear: Option[String],
+   discount: Option[Double],
+   competitor: Option[String],
+   age: Option[Double]) extends Serializable
 
 class TrainingData(
                     val customers: RDD[Customer]
